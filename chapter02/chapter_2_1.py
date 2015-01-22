d74de2c2e12a4aaf343f09e2c98c4fabcb5357dc
kiwi <moioutoi@163.com>
2015-01-22 08:33:38 -0500

    Good morning!

---
+++ chapter02/chapter_2_1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__author__ = 'kiwi'
+
+# This is the begining of my learning from Python :)
+print('''123 Let us go!''')  # ? why we cannot use double quote marks here
+print("J'aime %s" % 'Xqq')
+birthday_month = 'december'  # the month of the birthday month
+print('my birthday is in %s' % birthday_month)
+# the underscores(_) can be the first character in a variable name
+_underscore = '_'
+print('''this is underscore, %s''' % _underscore)
+legalBabyName = 'Meow '  # cambelCase for variable name
+print ('Cat', 1)
+dollars = 6
+print '1 RMB exnchanges to Canadian dollars is ', dollars, 'before'
+dollars = 4.99
+print 'but now is', dollars, '- _ -|||'
+# page 47
+print type(dollars)
+print type(1)  #?  why it shows <class'int'> in the book
