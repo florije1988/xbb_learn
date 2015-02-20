1ffaee49067dbb72ee4f71f4c3442cc2bcb15b61
kiwi <moioutoi@163.com>
2015-02-20 18:54:25 -0500

    私わ猫でし

---
+++ Chapter_3/chapter4.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+__author__ = 'kiwi'
+print 'chapter 4'   # page 117
+# if statement: first line is clause
+# boolean function: true or false '==' or  '!='
+def test_score(score):
+    if score > 95:
+        return 'Congratulate the user'  # statement after if is the statement's block when condition fittable
+    else:
+        return None
+
+# test_score(score=90)
+# test_score(score=98)
+'''
+page 125 checkpoint
+4.1 The control structure is : test statement for the lines of codes for running
+4.2 The decision structure is : test if the conditions is fittable for the way for executing in the next steps
+4.3 boolean structure : True in False
+4.4 The true and false statement for testing
+4.5 Relationship between the number for testing, equality operator, unequal operator ...  >, <, <=, >=, ==, !=
+4.6 if x == 0: ; if  y == 20:
+4.7 if commisionRate == 0.2 ; if sales >= 10000:
+'''
+
+
+
+
+
+
