0f494411242039526948822e71c3faecbc5482bf
kiwi <moioutoi@163.com>
2015-02-20 18:28:40 -0500

    私わ猫でし

--- Chapter_3/chapter3.py
+++ Chapter_3/chapter3.py
@@ -96,28 +96,191 @@ page 107 checkpoint
 3.16    the changes of value of parameters , yes it does when it is the pass by position
 3.17    a is the statements passes by position and b is the keyword arguments
 '''
-my_value = 10
+my_value = 10  # global name: set the global constant, which cannot be changes
 
 
 def print_out():
     print 'my value is: ', my_value
 
-print_out()
 
 def global_name():
-    global my_value
+    global my_value  # declare the number variable, when the value changes, my value below all changes
     my_value = int(raw_input('Input my value here: '))
     print 'The input my value is: ', my_value
     show_number()
 
+
 def show_number():
     print 'My value is: ', my_value
 
-global_name()
+
+# page 110
+# The following is used as a global constant; the contribution rate is the employee annual gross pay
+employee_annual_gross_pay = 0.05
 
 
+def total_pay():
+    global employee_annual_gross_pay
+    print 'The employee annual pay rate is: ', employee_annual_gross_pay * 100, '%'
+    employee_annual_pay = (employee_annual_gross_pay + 1) * \
+                          (float(raw_input('Please input the annual pay: $ ')))
+    print 'the annual pay is: ', employee_annual_pay
+    bonuses_paid = float(raw_input('Please input the bonuses  paid: $ '))
+    # bonuses to their retirement plans.
+    print 'The bonuses paid is: $ ', bonuses_paid
+    print format(bonuses_paid + employee_annual_pay, ',.2f')
+
+# call the function
+# total_pay()
+
+'''
+page 111-112 checkpoint
+3.18 The scope of a global variable is: the value which insert as the global value doesn't change at all during the
+program, it is the global value constant
+3.19 when the value needs to be changed more than once by the function
+3.20 The global constant is the global value in the whole program is constant , it is permissible to use global constant in a program
+
+Review Questions: ( Multiple Choice)    1~5: cbdbc   6~10: abaab    11~12:db
+True or False:  1~5: TTFFT    6~10: FTTTT
+
+Short answer:
+1. call the function repeatly by calling the function with the different parameter
+2. name the function is the naming the name of the function to be called; the first line is the header and the others
+are the statement
+3. the function stops running from the code
+4. Local variables are the values in the function, the statement in the function can use the values
+5. only in the function
+6. The global variable may be changes from the function but it is hard to find
+'''
+
+# Algorithm Workbench
+# 1
+def times_ten(number):
+    return number * 10
 
 
+# print 'The number times ten is: ', times_ten(4)
+
+# 2
+def show_value(quantity):
+    print quantity
+
+
+# show_value(quantity=12)
+'''
+3.  a=3,b=2,c=1
+4. (1,3.4)  (0,0)
+5. a. 2; b. a=2; b=4; c=6
+'''
+# Program Exercises
+# 1. kilometer converter
+
+
+def kiloeter_converter(kilometer_number):
+    return kilometer_number * 0.6214
+
+
+# print kiloeter_converter(kilometer_number=10000), 'miles'
+
+# 2 sale tax program refactoring
+def sale_tax():
+    sale = int(raw_input('Please input the sale price: '))
+    print 'sale: ', sale
+    tax_rate = 0.13  # 13%
+    print 'tax rate: ', tax_rate
+    total_sale = sale * (1 + tax_rate)
+    print 'total sale: ', total_sale
+
+
+#sale_tax()
+
+# 3 how much insurance?
+def min_insurance():
+    property_cost = float(raw_input('Please enter the number of property cost: '))
+    print 'The property cost inputed is: ', property_cost
+    amount_for_buy_insurance = float(property_cost / 0.8)
+    print 'Amount for insurance: ', amount_for_buy_insurance
+
+
+#min_insurance()
+
+# 4 Automobile Costs
+def monthly_cost():
+    print 'Please enter the price of loan payment, insurance, gas, oil, tires, and maintenance.'
+    loan = enter_the_number()
+    insurance = enter_the_number()
+    gas = enter_the_number()
+    oil = enter_the_number()
+    tires = enter_the_number()
+    maintenance = enter_the_number()
+    return loan + insurance + gas + oil + tires + maintenance
+
+
+def enter_the_number():
+    return raw_input('Please enter the number here: ')
+
+
+# monthly_cost()
+
+# 5 Property tax
+def property_tax():
+    print 'Please input the property value below'
+    property_value = enter_the_number()
+    actual_value = float(property_value) * (60 / 100.00)
+    print 'actual value is: ', actual_value
+    tax = 0.64 / 100.00 * actual_value
+    print 'The tax is: ', tax
+
+
+# property_tax()
+
+# 6. Body Mass Index
+def BMI(weight, height):
+    return weight * 703 / (height ** 2)
+
+#weight = float(raw_input("Please enter the weight here: "))
+#height = float(raw_input("Please enter the height here: "))
+# print BMI(weight=weight, height=height)
+
+# 7. Calories from Fat and Carbohydrates
+def evaluating_diets():
+    fat_grams = float(raw_input("Please enter the fat grams here: "))
+    carb_grams = float(raw_input("Please enter the carb grams here: "))
+    cal_fat = fat_grams * 9
+    cal_carbs = carb_grams * 4
+    print 'calories from fat', cal_fat
+    print 'calories from carbs', cal_carbs
+
+
+# evaluating_diets()
+# 8. Stadium Seating
+def seat_cost(num_A, num_B, num_C):
+    A = 15  # For a softball game, Class A seats cost $15, Class B seats cost $12, and Class C seats cost $9.
+    B = 12
+    C = 9
+    return num_A * A + num_B * B + num_C * C
+
+
+# print seat_cost(10, 20, 30)
+
+# 9. Paint Job Estimator
+def paint_estimator():
+    square = float(raw_input('Please enter the square feet of wall space to be painted '))
+    price = float(raw_input('The price of the paint per gallon '))
+    print 'The number of gallons of paint required', square *1.00 / 115
+    print 'The hours of labor required', square * 8.00 / 115
+    print 'The cost of the paint', price * square *1.00 / 115
+    print 'The labor charges', square * 8.00 * 12 / 115
+    print 'The total cost of the paint job', (square * 8.00 * 12 / 115) + (price * square *1.00 / 115)
+#paint_estimator()
+# 10. Monthly Sales Tax
+def monthly_sales_tax(sale):
+    state_sale_tax_rate = 0.04
+    country_sale_tax_rate = 0.02
+    print 'The amount of county sales tax', sale * 0.04
+    print 'The amount of state sales tax', sale * 0.02
+    print 'The total sales tax (county plus state)', sale * (0.04+0.02)
+#monthly_sales_tax(sale=1000)
 
 
 
