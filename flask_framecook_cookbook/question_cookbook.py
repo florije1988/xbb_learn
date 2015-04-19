ca5d2e3c18f4410ce8a122cf691ce83cc4526141
kiwi <moioutoi@163.com>
2015-04-19 01:14:36 -0400

    good-night~

--- flask_framecook_cookbook/question_cookbook.py
+++ flask_framecook_cookbook/question_cookbook.py
@@ -6,7 +6,9 @@ __author__ = 'kiwi'
 # page 8
 if __name == '__main__':
     app.run()
-''' Does the name based on the function app.run() or I can use the other like start.py to run this code by setting?'''
+''' Does the name based on the function app.run() or I can use the other like start.py to run this code by setting?
+    app = Flask(__name__)?? where is the Flask comes from and execute : need to import flask ??
+'''
 
 # page 12
 app.config.from_object(__name__)
