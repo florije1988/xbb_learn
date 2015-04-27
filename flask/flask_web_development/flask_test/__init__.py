6efa7b08583780fa8dbfb081e756956afd9f41b5
kiwi <moioutoi@163.com>
2015-04-27 14:13:47 -0400

    template

--- flask/flask_web_development/flask_test/__init__.py
+++ flask/flask_web_development/flask_test/__init__.py
@@ -2,16 +2,46 @@
 __author__ = 'kiwi'
 
 from flask import Flask
+from flask import make_response
+
 app = Flask(__name__)
 
-@app.route('/')
-def index(): # view function
-    return '<h1>Hello World!</h1>'
 
-@app.route('/user/<name>')
+@app.route('/')
+def index():  # view function
+    return '<h1>Hello World!</h1>', 400
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+@app.route('/')
+def index():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+
+@app.route('/user/<name>')  # display the name on it
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
-if __name__ == '__main__':   # Running on http://127.0.0.1:5000/ restarting with reloader
-    app.run(debug=True)
+from flask import redirect
+@app.route('/')
+def index():
+    return redirect('http://www.example.com')
 
+from flask import abort
+@app.route('/user/<id>')    # log in function
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+    return '<h1>Hello,%s</h1>' % name
+
+# for extension
+from flask.ext.script import Manager
+manager = Manager(app)
+
+
+if __name__ == '__main__':  # Running on http://127.0.0.1:5000/ restarting with reloader
+    app.run(debug=True)
